47facf662bc95bb81a543de42948731b65115d6c
Preetam Manna <preetammanna.cse@gmail.com>
2026-01-30 13:35:49 +0530

    fix cloudinary import for production

--- backend/controllers/pyqController.js
+++ backend/controllers/pyqController.js
@@ -1,5 +1,13 @@
 const PYQ = require("../models/PYQModel");
-const cloudinary = require("../utils/cloudinary");
+const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 
 // ================= ADMIN UPLOAD =================
 exports.uploadPYQByAdmin = async (req, res) => {
